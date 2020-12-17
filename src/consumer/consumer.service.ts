6a858318d20c0aba754498e49dd6b154c627dcd0
Ajay Prasad <ajay.prasad@goniyo.com>
2020-12-17 07:57:36 +0530

    reproduction code

--- src/consumer/consumer.service.ts
+++ src/consumer/consumer.service.ts
@@ -1,59 +1,25 @@
 import { Injectable } from "@nestjs/common";
 import { RabbitSubscribe } from "@golevelup/nestjs-rabbitmq";
 import { Rabbitmq } from "../enum/rabbitmq";
+import { PublishMessage } from "src/model/publish.message";
+import { Logger } from "src/logger/logger.decorator";
+import { LoggerSrv } from "src/logger/logger.service";
 @Injectable()
 export class ConsumerService {
-  constructor() {}
-  @RabbitSubscribe({
-    exchange: Rabbitmq.TOPIC_EXCHANGE_NAME,
-    routingKey: Rabbitmq.UPLOAD_DOC_ROUTING_KEY_NAME,
-    queue: Rabbitmq.UPLOAD_DOC_QUEUE_NAME,
-    queueOptions: {
-      durable: true,
-      exclusive: false,
-      autoDelete: false,
-    },
-  })
-  public async pubSubHandler(msg: {}) {
-    console.log(`Received message: ${JSON.stringify(msg)}`);
+  constructor(@Logger("ConsumerService") private logger: LoggerSrv) {
   }
-  @RabbitSubscribe({
+   @RabbitSubscribe({
     exchange: Rabbitmq.TOPIC_EXCHANGE_NAME,
-    routingKey: Rabbitmq.UPLOAD_DOC_ROUTING_KEY_NAME,
-    queue: Rabbitmq.UPLOAD_DOC_QUEUE_NAME,
+    routingKey: "upload.doc.routing.key",
+    queue: "test.queue",
     queueOptions: {
       durable: true,
       exclusive: false,
       autoDelete: false,
     },
-  })
-  public async pubSubHandler1(msg: {}) {
-    console.log(`Received message----1: ${JSON.stringify(msg)}`);
-  }
-  @RabbitSubscribe({
-    exchange: Rabbitmq.TOPIC_EXCHANGE_NAME,
-    routingKey: Rabbitmq.UPLOAD_DOC_ROUTING_KEY_NAME,
-    queue: Rabbitmq.UPLOAD_DOC_QUEUE_NAME,
-    queueOptions: {
-      durable: true,
-      exclusive: false,
-      autoDelete: false,
-    },
-  })
-  public async pubSubHandler2(msg: {}) {
-    console.log(`Received message----2: ${JSON.stringify(msg)}`);
-  }
-  @RabbitSubscribe({
-    exchange: Rabbitmq.TOPIC_EXCHANGE_NAME,
-    routingKey: Rabbitmq.UPLOAD_DOC_ROUTING_KEY_NAME,
-    queue: Rabbitmq.UPLOAD_DOC_QUEUE_NAME,
-    queueOptions: {
-      durable: true,
-      exclusive: false,
-      autoDelete: false,
-    },
-  })
-  public async pubSubHandler3(msg: {}) {
-    console.log(`Received message----3: ${JSON.stringify(msg)}`);
+    errorHandler:()=>{console.log("I am error")}
+  }) 
+  public  pubSubHandler(msg:PublishMessage) {
+    this.logger.log(`Received message: ${JSON.stringify(msg.payload)}`);
   }
 }
